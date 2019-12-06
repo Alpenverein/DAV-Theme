550b5d7b017cf91b2de4528b02ddf60e6d48c94d
Hauke <hauke.bendt@alpenverein.de>
2019-12-06 14:02:01 +0100

    Placing topNavigation by position rather than fixed name

--- header.php
+++ header.php
@@ -231,8 +231,7 @@ echo $menubehavior;
             </button>
             <div class="collapse navbar-collapse text-center justify-content-end" id="navbarDesktop">
 
-
-                <?php echo getDesktopMenu('Hauptmenue', $text_color, $parent, $current); ?>
+                <?php echo getDesktopMenu(get_term(get_nav_menu_locations()['primary'], 'nav_menu')->name, $text_color, $parent, $current); ?>
 
             </div>
         </div>
