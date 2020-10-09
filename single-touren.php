040a9eceba5dd10a1d3e42407d5a7686d884a7b4
Hauke Bendt <hauke.bendt@alpenverein.de>
2020-10-09 09:12:49 +0200

    own templates for persona and tour detailbox at tourpage

--- single-touren.php
+++ single-touren.php
@@ -27,117 +27,35 @@ if ((get_theme_mod('dav_breadcrumb') != false) && (get_theme_mod('dav_breadcrumb
 
     <div class="container">
         <div class="container-content">
-
-        <div class="row">
-            <div class="col-xs-12 col-sm-8">
-                <h1 style="margin-bottom: 5px;"><?php the_title(); ?></h1>
-                <span class="tour-data">
-
+            <div class="row">
+                <div class="col-xs-12 col-sm-8">
+                    <h1 style="margin-bottom: 5px;"><?php the_title(); ?></h1>
+                    <span class="tour-data">
                     <?php
-                    if ($tour_type != '') {echo '<strong>'.$tour_type.'</strong>';}
-                    if ($tour_category != '') {echo ' | '.'<strong>'.$tour_category.'</strong>';}
+                        if ($tour_type != '') {echo '<strong>'.$tour_type.'</strong>';}
+                        if ($tour_category != '') {echo ' | '.'<strong>'.$tour_category.'</strong>';}
                     ?>
-
-                </span>
-            </div>
-        </div>
-        <div class="row" style="margin-top: 15px;">
-            <div class="col-xs-12 col-sm-8" id="content">
-                <?php $tour_image = wp_get_attachment_image_src(get_field('acf_tourphoto'), 'medium'); ?>
-                <img src="<?php echo $tour_image[0] ?>" class="img-fluid" style="margin-bottom: 1em;">
-                <p class="lead text-primary"><?php echo $tour_meta['acf_tourcompact'][0]; ?></p>
-                <?php the_content(); ?>
-            </div>
-            <div class="col-sm-4">
-
-                <div class="card card-price bg-light mb-3">
-                    <div class="card-header bg-dark pt-1">
-                        <h2>Tourendetails</h2>
-                    </div>
-                    <div class="card-body">
-                        <div class="price-body pt-2">
-                            
-                            <p class="lead text-left"><strong><?php echo substr(get_field('acf_tourstartdate'),0,2).'.'.substr(get_field('acf_tourstartdate'),3,2).'.'.substr(get_field('acf_tourstartdate'),6,4);
-
-                                if((get_field('acf_tourstartdate') != '') && (get_field('acf_tourallday') == 1)) {
-
-                                    echo ' - ';
-                                    echo substr(get_field('acf_tourenddate'),0,2).'.'.substr(get_field('acf_tourenddate'),3,2).'.'.substr(get_field('acf_tourenddate'),6,4);
-                                }
-                                ?>
-                                
-                                </strong></p>
-
-                            <?php
-
-                            if (get_field('acf_tourtreffzeit') != "") {
-                                echo '<p class="lead"><strong>' . get_field("acf_tourtreffzeit") .' Uhr</strong></p>';
-                            }
-
-                            if (get_field('tour_intern') != "") {
-                                echo '<div class="d-flex mb-2"><div>Tourenkennung: <br><strong>'.get_field("tour_intern").' </strong></div></div>';
-                            }
-
-                            if (get_field('acf_tourtreffpunkt') != "") {
-                                echo '<div class="d-flex mb-2"><div>Treffpunkt: <br><strong>'.get_field("acf_tourtreffpunkt").' </strong></div></div>';
-                            }
-
-                            if (get_field('acf_tourtime') != "") {
-                                echo '<div class="d-flex mb-2"><div>Dauer: <br><strong>'.get_field("acf_tourtime").' h</strong></div></div>';
-                            }
-
-                            if (get_field('acf_tourkilometer') != "") {
-                                echo '<div class="d-flex mb-2"><div>Kilometer: <br><strong>'.get_field("acf_tourkilometer").'</strong></div></div>';
-                            }
-
-                            if (get_field('acf_tourhohenmeter') != "") {
-                                echo '<div class="d-flex mb-2"><div>Höhenmeter: <br><strong>'.get_field("acf_tourhohenmeter").'</strong></div></div>';
-                            }
-
-                            if ($tour_condition != "") {
-                                echo '<div class="d-flex mb-2"><div>Kondition: <br><strong>'.$tour_condition.'</strong></div></div>';
-                            }
-
-                            if ($tour_technic != "") {
-                                echo '<div class="d-flex mb-2"><div>Technik: <br><strong>'.$tour_technic.'</strong></div></div>';
-                            }
-
-                            ?>
-                        </div>
-                    </div>
+                    </span>
                 </div>
-
-                <?php if(get_field('acf_tourpersona') != '') { ?>
-                    
-                <div class="card bg-dark">
-                    <div class="row m-3 align-self-stretch">
-                        <div class="col-12 p-0"><?php echo get_the_post_thumbnail($persona->ID, 'persona-thumb', array('class' => 'img-fluid rounded-circle')) ?></div>
-                            <div class="col-12 p-0 pt-3 text-center">
-                                <span class="person-name"><a href="<?php echo get_permalink($persona)?>"><?php echo $persona->post_title ?></a></span><br />
-                                
-                                <?php if($persona_meta['persona_daten_funktion'][0] != ""){ ?>
-                                    <span class="person-title"><?php echo $persona_meta['persona_daten_funktion'][0] ?></span><br />
-                                <?php } ?>
-                                <?php if($persona_meta['persona_daten_telefon'][0] != ""){ ?>
-                                    <span class="person-title">Telefon: <a href="tel:<?php echo $persona_meta['persona_daten_telefon'][0] ?>"><?php echo $persona_meta['persona_daten_telefon'][0] ?></a></span><br />
-                                <?php } ?>
-                                <?php if($persona_meta['persona_daten_e_mail'][0] != ""){ ?>
-                                    <span class="person-title">Mail: <a href="mailto:<?php echo $persona_meta['persona_daten_e_mail'][0] ?>"><?php echo $persona_meta['persona_daten_e_mail'][0] ?></a></span><br />
-                                <?php } ?>
-                                <hr>
-                            </div>
-                        </div>
-                        <div class="row m-3" style="margin-top: 0 !important;">
-                            <div class="col-12 p-0 p-lg-0">
-                                <p><?php echo get_the_excerpt($persona) ?></p>
-                            </div>
-                        </div>
-                    </div>
+            </div>
+            <div class="row" style="margin-top: 15px;">
+                <div class="col-xs-12 col-sm-8" id="content">
+                    <?php $tour_image = wp_get_attachment_image_src(get_field('acf_tourphoto'), 'medium'); ?>
+                    <img src="<?php echo $tour_image[0] ?>" class="img-fluid" style="margin-bottom: 1em;">
+                    <p class="lead text-primary"><?php echo $tour_meta['acf_tourcompact'][0]; ?></p>
+                    <?php the_content(); ?>
                 </div>
-
-                <?php } ?>
-
-        </div>
+                <div class="col-sm-4">
+                    <?php set_query_var( 'tour_condition', $tour_condition ); ?>
+                    <?php set_query_var( 'tour_technic', $tour_technic ); ?>
+                    <?php get_template_part( 'template_snippets/tour-detailsbox', 'Tour-Details'); ?>
+                    <?php if(get_field('acf_tourpersona') != '') { ?>
+                        <?php set_query_var( 'persona', $persona ); ?>
+                        <?php set_query_var( 'persona_meta', $persona_meta ); ?>
+                        <?php get_template_part( 'template_snippets/persona-detailsbox', 'PersonaDetailBox'); ?>
+                    <?php } ?>
+                </div>
+            </div>
         </div>
     </div>
 
